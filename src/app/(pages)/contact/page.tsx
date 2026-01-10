83d04b26176b76cf757ffaebc84abcc9cdfbf2c2
Aayush Makwana <aayushmakwana16@gmail.com>
2026-01-10 13:32:56 +0530

    feat(json-ld): add json-ld structured data for seo enhancement

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -9,10 +9,47 @@ export const metadata: Metadata = {
   }
 }
 
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@graph': [
+    {
+      '@context': 'https://schema.org',
+      '@type': 'WebSite',
+      '@id': `${process.env.NEXT_PUBLIC_APP_URL}#website`,
+      name: 'Template Name',
+      description: 'Template Description',
+      url: `${process.env.NEXT_PUBLIC_APP_URL}`,
+      inLanguage: 'en-US'
+    },
+    {
+      '@context': 'https://schema.org',
+      '@type': 'WebPage',
+      '@id': `${process.env.NEXT_PUBLIC_APP_URL}#webpage`,
+      name: 'Contact Us',
+      description: "Get in touch with us. We're here to help and answer any questions you may have.",
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/contact`,
+      isPartOf: {
+        '@id': `${process.env.NEXT_PUBLIC_APP_URL}#website`
+      },
+      potentialAction: {
+        '@type': 'ReadAction',
+        target: [`${process.env.NEXT_PUBLIC_APP_URL}/contact`]
+      }
+    }
+  ]
+}
+
 const ContactUsPage = () => {
   return (
     <div className='p-6'>
       <h1>Contact Us Page</h1>
+      {/* Add JSON-LD to your page */}
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, '\\u003c')
+        }}
+      />
     </div>
   )
 }
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,7 +1,29 @@
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@graph': [
+    {
+      '@context': 'https://schema.org',
+      '@type': 'WebSite',
+      '@id': `${process.env.NEXT_PUBLIC_APP_URL}#website`,
+      name: 'Template Name',
+      description: 'Template Description',
+      url: `${process.env.NEXT_PUBLIC_APP_URL}`,
+      inLanguage: 'en-US'
+    }
+  ]
+}
+
 const Home = () => {
   return (
     <div className='p-6'>
       <h1>Home Page</h1>
+      {/* Add JSON-LD to your page */}
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, '\\u003c')
+        }}
+      />
     </div>
   )
 }
