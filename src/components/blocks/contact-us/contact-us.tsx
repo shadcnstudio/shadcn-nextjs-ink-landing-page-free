88528daebba4cdb69b797671983449444c030212
Vatsal6288 <kachhadiyavatsal1@gmail.com>
2026-01-29 13:37:29 +0530

    refactor(contact): updated contact-us page

--- src/components/blocks/contact-us/contact-us.tsx
+++ src/components/blocks/contact-us/contact-us.tsx
@@ -4,7 +4,7 @@ import ContactForm from '@/components/blocks/contact-us/contact-form'
 
 const ContactUs = () => {
   return (
-    <section className='bg-muted py-8 sm:py-16 lg:py-16'>
+    <section className='bg-muted py-8 sm:py-16 lg:h-dvh lg:py-32'>
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
         <div className='mb-12 space-y-4 text-center sm:mb-16'>
           <h2 className='text-2xl font-semibold md:text-3xl lg:text-4xl'>Get in touch with us for more information</h2>
